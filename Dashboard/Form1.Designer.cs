f6a2e04545fd32f8b102181f0a06de8dde16d57f
LAPTOP-D1JDJ1G9\metairie <metairie@ebu.ch>
2018-05-10 11:44:37 +0200

    1st

---
+++ Dashboard/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Dashboard
+{
+    partial class main
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.sidepanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.menu = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.headerpanel = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sidepanel.SuspendLayout();
+            this.menu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.headerpanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sidepanel
+            // 
+            this.sidepanel.BackColor = System.Drawing.Color.DimGray;
+            this.sidepanel.Controls.Add(this.menu);
+            this.sidepanel.Controls.Add(this.button1);
+            this.sidepanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.sidepanel.Location = new System.Drawing.Point(0, 0);
+            this.sidepanel.Margin = new System.Windows.Forms.Padding(0);
+            this.sidepanel.Name = "sidepanel";
+            this.sidepanel.Size = new System.Drawing.Size(201, 721);
+            this.sidepanel.TabIndex = 0;
+            // 
+            // menu
+            // 
+            this.menu.BackColor = System.Drawing.Color.SkyBlue;
+            this.menu.Controls.Add(this.label1);
+            this.menu.Controls.Add(this.pictureBox1);
+            this.menu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Margin = new System.Windows.Forms.Padding(0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(200, 54);
+            this.menu.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Dashboard";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(40, 33);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DimGray;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(0, 54);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(198, 50);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Home";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // headerpanel
+            // 
+            this.headerpanel.BackColor = System.Drawing.Color.White;
+            this.headerpanel.Controls.Add(this.pictureBox2);
+            this.headerpanel.Controls.Add(this.label4);
+            this.headerpanel.Controls.Add(this.label3);
+            this.headerpanel.Controls.Add(this.label2);
+            this.headerpanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.headerpanel.Location = new System.Drawing.Point(201, 0);
+            this.headerpanel.Margin = new System.Windows.Forms.Padding(0);
+            this.headerpanel.Name = "headerpanel";
+            this.headerpanel.Size = new System.Drawing.Size(947, 54);
+            this.headerpanel.TabIndex = 1;
+            this.headerpanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.headerpanel_MouseDown);
+            this.headerpanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.headerpanel_MouseMove);
+            this.headerpanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.headerpanel_MouseUp);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(25, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(37, 31);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label4.Location = new System.Drawing.Point(68, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 32);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Home";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label3.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label3.Location = new System.Drawing.Point(902, 0);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(0, 6, 3, 0);
+            this.label3.Size = new System.Drawing.Size(19, 28);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "-";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(921, 0);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(0, 6, 3, 0);
+            this.label2.Size = new System.Drawing.Size(26, 28);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "X";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1148, 721);
+            this.Controls.Add(this.headerpanel);
+            this.Controls.Add(this.sidepanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "main";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.main_Load);
+            this.sidepanel.ResumeLayout(false);
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.headerpanel.ResumeLayout(false);
+            this.headerpanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel sidepanel;
+        private System.Windows.Forms.Panel menu;
+        private System.Windows.Forms.Panel headerpanel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Dashboard/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dashboard
+{
+    public partial class main : Form
+    {
+        int mouseX = 0, mouseY = 0;
+        bool mouseDown;
+
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void headerpanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            mouseX = e.X;
+            mouseY = e.Y;
+        }
+
+        private void headerpanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown) {
+                this.SetDesktopLocation(MousePosition.X - mouseX - menu.Size.Width, MousePosition.Y - mouseY);
+            }
+        }
+
+        private void headerpanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+        private void main_Load(object sender, EventArgs e)
+        {
+            this.Location = Screen.AllScreens[1].WorkingArea.Location;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        /*int headerX = this.headerpanel.Left;
+string message = " mouse X relative = " + (MousePosition.X - headerX) + " mouseX =" + MousePosition.X + " left " +  this.Left;
+string caption = "Error Detected in Input";
+MessageBoxButtons buttons = MessageBoxButtons.OK;
+DialogResult result;
+result = MessageBox.Show(message, caption, buttons);*/
+
+    }
+}
